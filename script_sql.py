032a6ac63addc1c79a5abced885efdb18f271f7b
Maksim Artemev <dan-pro352@ya.ru>
2018-08-16 00:22:31 +0300

    Update insert function

--- script_sql.py
+++ script_sql.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#put in cmd.exe: chcp 65001 (utf-8)
 import requests
 import json
 import psycopg2
@@ -16,23 +17,17 @@ def connect(dbname, user, host, password):
         return 'Connected! \n'
         global cur
 
-def record_stats():
-    main_list = []
-    for l in json_metrics['data']:
-        list_dim = []
-        list_met = []
-        for k in l['dimensions']:
-            list_dim.append(k['name'])
-        for p in l['metrics']:
-            list_met.append(p)
-        sql = """INSERT INTO stats (id, keyword, searchsystem, visit, bouncerate, deeppage, visittime) VALUES (DEFAULT, '%s', '%s', '%s', '%s', '%s', '%s')""" % (list_dim[0], list_dim[1], list_met[0], list_met[2], list_met[3], list_met[4])
-        return sql
-        try:
-            cur.execute(sql)
-            conn.commit()
-            return 'SQL Success!'
-        except:
-            return 'I can\'t write!'
+def insert(keyword, searchsystem, visit, bouncerate, deeppage, visittime):
+    query = """INSERT INTO stats (id, keyword, searchsystem, visit, bouncerate, deeppage, visittime) VALUES (DEFAULT, '%s', '%s', '%s', '%s', '%s', '%s')""" % (keyword, searchsystem, visit, bouncerate, deeppage, visittime)
+    return (query)
+
+def execute_sql(sql):
+    try:
+        cur.execute(sql)
+        conn.commit()
+        return 'SQL Success!'
+    except:
+        return 'I can\'t write!'
     cur.close()
 
 def view_stats(sql_query):
@@ -45,9 +40,17 @@ def close_con():
 
 
 if __name__ == "__main__":
-    #print(parsing_html('https://api-metrika.yandex.ru/stat/v1/data?preset=sources_search_phrases&limit=10000&pretty=true&date1=2015-08-10&date2=2018-08-11&id=23220061&oauth_token='))
-    print(connect('panel', 'postgres', 'localhost', ''))
+    html = parsing_html('https://api-metrika.yandex.ru/stat/v1/data?preset=sources_search_phrases&limit=10000&pretty=true&date1=2015-08-10&date2=2018-08-11&id=23220061&oauth_token=AQAAAAACPju0AAUlKa2hb5GQsEVOrhj984J9NYk')
+    for l in html['data']:
+        list_dim = []
+        list_met = []
+        for k in l['dimensions']:
+            list_dim.append(k['name'])
+        for p in l['metrics']:
+            list_met.append(p)
+        print(insert(list_dim[0], list_dim[1], list_met[0], list_met[2], list_met[3], list_met[4]))
+    '''print(connect('panel', 'postgres', 'localhost', ''))
     sql_elements = view_stats("""SELECT * FROM stats WHERE searchsystem = 'Яндекс' LIMIT 10""")
     for elements in rows:
         print(elements)
-    close_con()
+    close_con()'''
